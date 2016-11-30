f979f595c8ea5259e098d138e83321e4935150f4
Junior Quevedo Gutierrez <juniorquegu@gmail.com>
2016-11-30 01:03:07 -0500

    Create ServiceFactory.java

---
+++ ServiceFactory.java
@@ -0,0 +1,18 @@
+/**
+ * Service Factory for Retrofit
+ */
+public class ServiceFactory {
+
+    public static final String API_BASE_URL =  ApiConstants.BASE;
+
+    private static OkHttpClient httpClient = new OkHttpClient();
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient).build();
+        return retrofit.create(serviceClass);
+    }
+}
